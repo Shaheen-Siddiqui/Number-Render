fd0c405593cdedf418ecc89da3b3cacbe0c4f589
shaheen-siddiqui <shanvisiddya@123>
2022-02-03 16:22:03 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,42 @@
 import React, { useState } from 'react';
 
-
 function App() {
   let [Inpval, setInpval] = useState();
+  let [btn,setBtn] = useState(false)
 
   const inputFac = ((event) => {
     const value = event.target.value;
+    
   })
-
+const clicking=()=>{
+  setBtn(true)
+}
+const dblclick=()=>{
+  setBtn(false)
+}
 
 
   return (
     <>
 
-      <input className='inputFild' autoFocus placeholder='Enter Your Email..' onChange={inputFac} /><br />
-      <button className='btn'>click for Result</button>
+      <input className='inputFild' autoFocus placeholder='Enter Your Email..' onChange={inputFac}/><br />
+      <button className='btn' onClick={clicking}  >click for Result</button>
+      <br/>
+      <br/>
+      <br/>
 
-      <div class="card" style={{ width: "18rem", backgroundColor:"pink" }}>
-        <div class="card-header">
-          Featured
-        </div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">An item</li>
-          <li class="list-group-item">A second item</li>
-          <li class="list-group-item">A third item</li>
-        </ul>
-      </div>
+   {btn? <div className="outer" DoubleClick={dblclick}>
+      <ol>
+        <li>1234567789 aadil ki ammi</li>
+        <li>1234567789 aadil ki abbu</li>
+        <li>1234567789 aadil ki khala</li>
+        <li>1234567789 aadil ki fufi</li>
+        <li>1234567789 aadil ki bahan</li>
+        <li>1234567789 aadil ki bhaijaan</li>
+        <li>1234567789 aadil ki bahan</li>
+
+      </ol>
+    </div> :null}
 
     </>
   );
